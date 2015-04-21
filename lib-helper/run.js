88047c195108e7c24f006403860ad6c107a0bbdf
daidi <daidi88@sina.com>
2015-04-21 22:36:59 +0800

    ver 5.0

--- lib-helper/run.js
+++ lib-helper/run.js
@@ -4,30 +4,35 @@ var icon;
 var title;
 chrome.extension.sendRequest({
 	method : "getSchool"
-}, function(response) {
+}, function (response) {
 	var data = JSON.parse(response.data);
 	var school = data.school;
-    var source = data.source;
-	getBookInfo(school, source);
-  }
-);
-function getBookInfo(school, source) {
-console.log(TYPE+isbn+title+school+source);
-addInfo(TYPE,school);
-	$.ajax({
-		url : 'http://'+source+'/opac/openlink.php?strSearchType=isbn&strText='+isbn+'&showmode=table',
-		success : function (loginstr) {
-		if (loginstr.indexOf('本馆没有您检索的馆藏书目') != -1) {
-						$('#isex').html('   我大'+school+'竟然没有这本书！<a href="http://'+source+'/asord/asord_redr.php?title='+title+'">点击荐购</>');
-					}
-			$.ajax({
-				url : 'http://'+source+'/opac/item.php?marc_no='+loginstr.split('marc_no=')[1].split('">')[0],
-				type : 'GET',
-				success : function (msg) {
+	var source = data.source;
+	var system = data.system;
+	getBookInfo(school, source, system);
+});
+function getBookInfo(school, source, system) {
+	console.log(TYPE + isbn + title + school + source + system);
+	//system定义
+	//0：未匹配
+	//1：旧版汇文
+	//2：新版汇文
+	addInfo(TYPE, school);
+	if (system == 1) {
+		$.ajax({
+			url : 'http://' + source + '/opac/openlink.php?strSearchType=isbn&strText=' + isbn + '&showmode=table',
+			success : function (loginstr) {
+				if (loginstr.indexOf('本馆没有您检索的馆藏书目') != -1) {
+					$('#isex').html('   我' + school + '竟然没有这本书！<a href="http://' + source + '/asord/asord_redr.php?title=' + title + '">点击荐购</>');
+				}
+				$.ajax({
+					url : 'http://' + source + '/opac/item.php?marc_no=' + loginstr.split('marc_no=')[1].split('">')[0],
+					type : 'GET',
+					success : function (msg) {
 						$('#isex').html('开玩笑！怎么可能没有！');
 						var doc_number = msg.split('marc_no=')[1].split('">')[0];
 						$('#isex').after('<br><h2 style="color: #007722;">在哪借这本书?</h2>');
-						var findurl = 'http://'+source+'/opac/item.php?marc_no='+doc_number;
+						var findurl = 'http://' + source + '/opac/item.php?marc_no=' + doc_number;
 						$.ajax({
 							url : findurl,
 							type : 'GET',
@@ -52,76 +57,101 @@ addInfo(TYPE,school);
 								}
 							}
 						});
+					}
+
+				});
+			}
+		});
+	} else if (system == 2) {
+		$.ajax({
+			url : 'http://' + source + '/opac/openlink.php?strSearchType=isbn&strText=' + isbn + '&showmode=table',
+			success : function (loginstr) {
+				if (loginstr.indexOf('本馆没有您检索的图书') != -1) {
+					$('#isex').html('<br><a href="http://' + source + '/asord/asord_redr.php?title=' + title + '">'+school+'图书馆竟然没有这本书！点击荐购</><br>');
+				} else {
+					var marc_no = loginstr.split('marc_no=')[1].split('">')[0];
+					var findurl = 'http://' + source + '/opac/item.php?marc_no=' + marc_no
+						$.ajax({
+							url : findurl,
+							type : 'GET',
+							success : function (detail) {
+								var detail_table = '<br><table style="border:2px solid #C1DAD7 ;border-spacing: 0px; padding: 0;" id="daidires">' + detail.split('id="item">')[1].split('</table>')[0].replace(new RegExp("img", "gm"), "p") + '</table><br><a href="' + findurl + '" target="_blank">点击跳转到图书馆对应界面</a>';
+								$('#daidilib').append(detail_table);
+								var css = '';
+								$('#daidires td').css({
+									'border' : '2px solid #C1DAD7',
+									'font-size' : ' 11px',
+									'padding' : '6px 6px 6px 12px',
+									'color' : '#4f6b72'
+								});
+							}
+						});
 				}
-				
-			});
-		}
-	});
+
+			}
+		});
+	}
+
 }
 function getType() {
-    var domain = document.domain;
+	var domain = document.domain;
 	console.log(domain);
-    var names = ["amazon", "douban", "jd", "dangdang"];
-    for (var i in names) {
-        if (domain.indexOf(names[i]) != -1)		
-            return names[i];
-    }
-	
+	var names = ["amazon", "douban", "jd", "dangdang"];
+	for (var i in names) {
+		if (domain.indexOf(names[i]) != -1)
+			return names[i];
+	}
+
 }
 function getIsbn() {
-    switch (TYPE) {
-        case 'douban':
-			isbn = $('#info').text().split('ISBN:')[1].split(' ')[1].trim();
-			title= $('#wrapper').text().split('作者')[0].trim();
-            icon = $('div#mainpic img').attr('src');
-            break;
-        case 'amazon':
-            var items = $('div.content b');
-            var isbntemp;
-            var i;
-            for (i = 0; i < items.length; i++) {
-                if (items[i].innerText == "ISBN:") {
-                    isbook = 1;
-                }
-                if (items[i].innerText == "条形码:") {
-                    var isbntemp = items[i].nextSibling.nodeValue.trim();
-                }
-            }
-            if (isbntemp && isbook)
-                var isbn = isbntemp;
-            icon = $("#prodImageCell img").attr("src");
-            title = $("#btAsinTitle").text();
-            title = title.split(' [')[0];
-            break;
-        case 'dangdang':
-            isbn =$('.show_info').text().split('N：')[1].split('包')[0].trim();
-            break;
-        case 'jd':
-            isbn = $("#summary li:eq(10)").text().split('Ｎ：')[1].trim();
-            break;
-        default:
-    }
-    return isbn;
+	switch (TYPE) {
+	case 'douban':
+		isbn = $('#info').text().split('ISBN:')[1].split(' ')[1].trim();
+		title = $('#wrapper h1').text().trim();
+		icon = $('div#mainpic img').attr('src');
+		break;
+	case 'amazon':
+		var items = $('tr div.content ul').text();
+		var isbntemp;
+		var i = items.indexOf("ISBN");
+		if (items.indexOf("ISBN") != -1) {
+			var isbn = items.substring(i + 5, i + 18)
+		}
+		title = $("#productTitle").text();
+		break;
+	case 'dangdang':
+		
+        if($('.book_messbox').length > 0)
+            isbn = $('.book_messbox').text().split('Ｎ')[1].trim().split('\n')[0].replace(/[^0-9]/g,'');
+        else
+            isbn = $('#detail_all').text().split('Ｎ')[1].split('\n')[0].split(' ')[0].replace(/[^0-9]/g,'');
+		break;
+	case 'jd':
+		isbn = $("#parameter2").text().split('ISBN：')[1].split('\n')[0].trim();
+		break;
+	default:
+	}
+	return isbn;
 }
-function addInfo(TYPE,school) {
-    switch (TYPE) {
-        case 'douban':
-			$('#buyinfo').before('<div class="gray_ad" id="daidilib"></div>');
-			$('#daidilib').append('<h2>'+school+'图书馆有没有?</h2><div class="bs" id="isex"></div>');
-            break;
-        case 'amazon':
-            $('.twisterMediaMatrix').after('<hr/><div class="buyingDetailsGrid" id="daidilib"></div><hr/>'); 
-            $('#daidilib').append('<h2 style="color: #007722;">'+school+'图书馆有没有?</h2><div class="bs" id="isex"></div>');
-            break;
-        case 'dangdang':
-            $('.sale').after('<div class="buy_area" id="daidilib"></div>');
-            $('#daidilib').append('<h2 style="color: #007722;">'+school+'图书馆有没有?</h2><div class="bs" id="isex"></div>');
-            break;
-        case 'jd':
-            $('#choose').before('<hr/><div id="daidilib" class="btns"></div><hr/>');
-            $('#daidilib').append('<h1 style="color: #007722;">'+school+'图书馆有没有?</h2><div class="bs" id="isex"></div>');
-            break;
-        default:
-    }
+function addInfo(TYPE, school) {
+	switch (TYPE) {
+	case 'douban':
+		$('.related_info').before('<div class="gray_ad" id="daidilib"></div>');
+		$('#daidilib').append('<h2>' + school + '图书馆馆藏信息</h2><small>powered by 图书馆助手</small><div class="bs" id="isex"></div>');
+		break;
+	case 'amazon':
+		$('#dynamicDeliveryMessage_feature_div').before('<div class="a-box rbbSection"><div class="a-box-inner" id="daidilib"></div></div><hr/>');
+		$('#daidilib').append('<h2 style="color: #007722;">' + school + '图书馆馆藏信息</h2><small>powered by 图书馆助手</small><div class="bs" id="isex"></div>');
+		break;
+	case 'dangdang':
+		$('.sale').after('<div style="border: 1px dotted #785;background: #f5f5f5;padding: 10px;margin: 30px 0 30px 0;" id="daidilib"></div>');
+		$('#daidilib').append('<h2 style="color: #f44700;font-weight: normal;">' + school + '图书馆馆藏信息</h2><small>powered by 图书馆助手</small><div class="bs" id="isex"></div>');
+		break;
+	case 'jd':
+		$('#choose').before('<hr/><div id="daidilib" class="btns"></div><hr/>');
+		$('#daidilib').append('<h2 style="color: #007722;">' + school + '图书馆馆藏信息</h2><small>powered by 图书馆助手</small><hr/><div class="bs" id="isex"></div>');
+		break;
+	default:
+	}
 
 }
--- lib-helper/script.js
+++ lib-helper/script.js
@@ -1,44 +1,89 @@
-/* Author:
-
-*/
 var notificationCheckbox = $('#notification-checkbox');
 var school = $('#school');
-var option = $('option[selected]');
+var system = $('#system');
+var source = $('#source');
 
-$(document).ready(function(){
-  var myschool = localStorage.getItem('my_school');  
-  if (myschool) {
-    $("#school option[value="+myschool+"]").attr('selected', 'selected');
-	} 
+$(document).ready(function () {
+	var myschool = localStorage.getItem('my_school');
+	if (myschool) {
+		$('#school').val(myschool);
+	}
+});
+$("#school").on('click', function () {
+	makeCenter();
+	initProvince();
+	$('[province-id="1"]').addClass('choosen');
+	initSchool(1);
 });
 
+$("#close-button").on('click', function () {
+	$('#choose-box-wrapper').css("display", "none");
+}
+	function initProvince() {
+	$('#choose-a-province').html('');
+	for (i = 0; i < schoolList.length; i++) {
+		$('#choose-a-province').append('<a class="province-item" province-id="' + schoolList[i].id + '">' + schoolList[i].name + '</a>');
+	}
+	$('.province-item').bind('click', function () {
+		var item = $(this);
+		var province = item.attr('province-id');
+		var choosenItem = item.parent().find('.choosen');
+		if (choosenItem)
+			$(choosenItem).removeClass('choosen');
+		item.addClass('choosen');
+		//更新大学列表
+		initSchool(province);
+	});
+}
+	function initSchool(provinceID) {
+	//原先的学校列表清空
+	$('#choose-a-school').html('');
+	var schools = schoolList[provinceID - 1].school;
+	for (i = 0; i < schools.length; i++) {
+		$('#choose-a-school').append('<a class="school-item" school-name="' + schools[i].name + '" school-url="' + schools[i].url + '" school-sys="' + schools[i].system + '">' + schools[i].name + '</a>');
+	}
+	//添加大学列表项的click事件
+	$('.school-item').bind('click', function () {
+		var item = $(this);
+		var school = item.attr('school-name');
+		var url = item.attr('school-url');
+		var system = item.attr('school-sys');
+		//更新选择大学文本框中的值
+		$('#source').val(url);
+		$('#system').val(system);
+		$('#school').val(school);
+		//关闭弹窗
+		hide();
+	});
+}
+	function makeCenter() {
+	$('#choose-box-wrapper').css("display", "block");
+	$('#choose-box-wrapper').css("position", "absolute");
+	$('#choose-box-wrapper').css("top", Math.max(0, (($(window).height() - $('#choose-box-wrapper').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+	$('#choose-box-wrapper').css("left", Math.max(0, (($(window).width() - $('#choose-box-wrapper').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+}
 
-$("#save-button").on('click', function() {
-  var school_id = school.val();
-  var source = $('option[selected]').data('source');
-  var olddata = localStorage.getItem("data");
-  if(olddata){
-  var oldschool=olddata.split('source')[0];
-  var oldsource=olddata.split('source')[1];
-  }
-  if(school_id!="暂无")
-  {
-  var data = {"school": school_id, "source": source };
-  localStorage.setItem('my_school', school_id);
-  localStorage.setItem('data', JSON.stringify(data));
-  var newdata = localStorage.getItem("data");
-  var newschool=newdata.split('source')[0];
-  var newsource=newdata.split('source')[1];
-  if(newsource==oldsource)
-  {
-    if(newschool!=oldschool)
-		alert('由于chrome内核追求极致速度，省略了一些dom的属性的加载,您需要再点击一次保存确认！');
-		else
-		alert('您的学校信息没有发生变化！');
-  } 
-  else
-  alert('信息保存成功！您可以到本页面最底部我们提供的测试页面查看效果！');
-  }
-  else
-  alert('对不起，您的学校尚未添加支持！由于本人精力有限，适配学校速度进度极慢，但是对于单独邮件申请，一般会立刻更新。所以烦请请发送邮件到daidi88@sina.com，让我知道你在这里！');
-});
+	$("#save-button").on('click', function () {
+		var _school = school.val();
+		var _source = source.val();
+		var _system = system.val();
+
+		var olddata = JSON.parse(localStorage.getItem("data"));
+		var oldschool = olddata.school;
+		if (_school == oldschool) {
+			alert('信息没有变化！');
+			return;
+		}
+		if (_system != 0) {
+			var data = {
+				"school" : _school,
+				"source" : _source,
+				"system" : _system
+			};
+			localStorage.setItem('my_school', _school);
+			localStorage.setItem('data', JSON.stringify(data));
+			alert('信息保存成功！您可以到本页面最底部我们提供的测试页面查看效果！');
+		} else {
+			alert('对不起，您的学校尚未添加支持！由于本人精力有限，适配学校速度进度极慢，但是对于单独邮件申请，一般会立刻更新。所以烦请请发送邮件到daidi88@sina.com，让我知道你在这里！');
+		}
+	});
